65b64483c540c7ae709bcd22e15345e80af6995a
Andy Zhou <zhirui1977@163.com>
2021-10-04 17:00:31 +0800

    add client api

---
+++ client.go
@@ -0,0 +1,236 @@
+package cree
+
+import (
+	"errors"
+	"fmt"
+	"github.com/andyzhou/cree/face"
+	"log"
+	"net"
+)
+
+/*
+ * face for client
+ * @author <AndyZhou>
+ * @mail <diudiu8848@163.com>
+ */
+
+//inter default value
+const (
+	TcpReadBuffSize = 1024
+	LazySendChanSize = 1024
+)
+
+//face info
+type Client struct {
+	host string
+	port int
+	readBuffSize int
+	conn *net.Conn
+	connected bool
+	cbForRead func(data []byte) bool
+	lazySendChan chan []byte
+	closeChan chan bool
+}
+
+//construct
+func NewClient(
+			host string,
+			port int,
+			lazyChanSize ...int,
+		) *Client {
+	var (
+		lazySendChanSize int
+	)
+
+	//setup option parameter
+	lazySendChanSize = LazySendChanSize
+	if lazyChanSize != nil && len(lazyChanSize) > 0 {
+		lazySendChanSize = lazyChanSize[0]
+	}
+
+	//self init
+	this := &Client{
+		host: host,
+		port: port,
+		readBuffSize: TcpReadBuffSize,
+		lazySendChan: make(chan []byte, lazySendChanSize),
+		closeChan: make(chan bool, 1),
+	}
+	go this.runLazySendProcess()
+	return this
+}
+
+//close connect
+func (c *Client) Close() {
+	if c.conn == nil {
+		return
+	}
+	(*c.conn).Close()
+	c.conn = nil
+	c.connected = false
+	c.closeChan <- true
+	close(c.lazySendChan)
+}
+
+//set cb for read data
+func (c *Client) SetCBForRead(cb func(data []byte) bool) bool {
+	if cb == nil {
+		return false
+	}
+	c.cbForRead = cb
+	return true
+}
+
+//set read buff size
+func (c *Client) SetReadBuffSize(size int) bool {
+	if size <= 0 {
+		return false
+	}
+	c.readBuffSize = size
+	return true
+}
+
+//send packet data
+func (c *Client) SendPacket(
+					messageId uint32,
+					data []byte,
+					lazySend ...bool,
+				) error {
+	var (
+		isLazySend bool
+	)
+
+	//check
+	if messageId < 0 || data == nil {
+		return errors.New("invalid parameter")
+	}
+	if c.conn == nil {
+		return errors.New("connect is nil")
+	}
+
+	//packet data
+	packet := c.packetData(messageId, data)
+
+	//check lazy mode
+	if lazySend != nil && len(lazySend) > 0 {
+		isLazySend = lazySend[0]
+	}
+
+	//try send to server
+	if isLazySend {
+		select {
+		case c.lazySendChan <- packet:
+		}
+	}else{
+		//send direct
+		_, err := (*c.conn).Write(packet)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//connect server
+func (c *Client) ConnServer() error {
+	//check
+	if c.host == "" || c.port <= 0 {
+		return errors.New("host or port is invalid")
+	}
+	if c.connected {
+		return nil
+	}
+
+	//format address
+	address := fmt.Sprintf("%s:%d", c.host, c.port)
+
+	//try connect server
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+
+	//sync conn
+	c.conn = &conn
+	c.connected = true
+
+	//spawn read process
+	go c.runReadProcess()
+
+	return nil
+}
+
+////////////////
+//private func
+////////////////
+
+//packet one data
+func (c *Client) packetData(messageId uint32, data []byte) []byte {
+	message := face.NewMessage()
+	message.Id = messageId
+	message.SetData(data)
+	packet := face.NewPacket()
+	byteData, _ := packet.Pack(message)
+	return byteData
+}
+
+//lazy send process
+func (c *Client) runLazySendProcess() {
+	var (
+		packet []byte
+		isOk bool
+	)
+
+	//defer
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Client:runLazySendProcess panic, err:", err)
+		}
+		close(c.closeChan)
+	}()
+
+	//loop
+	for {
+		select {
+		case packet, isOk = <- c.lazySendChan:
+			if isOk {
+				(*c.conn).Write(packet)
+			}
+		case <- c.closeChan:
+			return
+		}
+	}
+}
+
+//read process
+func (c *Client) runReadProcess() {
+	var (
+		buff []byte
+		err error
+	)
+
+	//init buff
+	buff = make([]byte, c.readBuffSize)
+
+	//defer
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Client:runReadProcess panic, err:", err)
+		}
+		c.Close()
+	}()
+
+	//loop
+	for {
+		//try read tcp data
+		_, err = (*c.conn).Read(buff)
+		if err != nil {
+			panic(err)
+		}
+
+		//call cb
+		if c.cbForRead != nil {
+			c.cbForRead(buff)
+		}
+	}
+}
--- example/client.go
+++ example/client.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/andyzhou/cree/face"
-	"net"
+	"github.com/andyzhou/cree"
+	"log"
+	"os"
 	"sync"
 	"time"
 )
@@ -14,57 +15,74 @@ import (
  * @mail <diudiu8848@163.com>
  */
 
- //pack test data
-func packTestData(clientId int) []byte {
-	data := fmt.Sprintf("clientId:%d, time:%d", clientId, time.Now().Unix())
-	message := face.NewMessage()
-	message.Id = 1
-	message.SetData([]byte(data))
-	packet := face.NewPacket()
-	byteData, _ := packet.Pack(message)
-	return byteData
+// //pack test data
+//func packTestData(clientId int) []byte {
+//	data := fmt.Sprintf("clientId:%d, time:%d", clientId, time.Now().Unix())
+//	message := face.NewMessage()
+//	message.Id = 1
+//	message.SetData([]byte(data))
+//	packet := face.NewPacket()
+//	byteData, _ := packet.Pack(message)
+//	return byteData
+//}
+//
+//func CreateOneClient(address string, id int)  {
+//	conn, err := net.Dial("tcp", address)
+//	if err != nil {
+//		fmt.Println("client start failed, err:", err.Error())
+//		return
+//	}
+//	i := 1
+//	for {
+//		//init packet
+//		packTestData := packTestData(id)
+//
+//		//send
+//		_, err := conn.Write(packTestData)
+//		if err != nil {
+//			break
+//		}
+//
+//		//read
+//		buf := make([]byte, 512)
+//		cnt, err := conn.Read(buf)
+//		if err != nil {
+//			fmt.Println("read buf error ")
+//			return
+//		}
+//		fmt.Printf(" server call back : %s, cnt = %d\n", string(buf), cnt)
+//		if i >= 50 {
+//			//up to limit of testing
+//			break
+//		}
+//		time.Sleep(time.Second * 1)
+//		i++
+//	}
+//	conn.Close()
+//}
+//
+//func ClientTest(host string, port, clients int) {
+//	time.Sleep(time.Second * 3)
+//	addr := fmt.Sprintf("%s:%d", host, port)
+//	for i := 1; i <= clients; i++ {
+//		go CreateOneClient(addr, i)
+//	}
+//}
+
+//cb for client read
+func CBForRead(data []byte) bool {
+	log.Println("client:CBForRead, data:", string(data))
+	return true
 }
 
-func CreateOneClient(address string, id int)  {
-	conn, err := net.Dial("tcp", address)
+//test write
+func ClientWrite(client *cree.Client) {
+	//send packet data
+	messageId := uint32(1)
+	data := fmt.Sprintf("time:%d", time.Now().Unix())
+	err := client.SendPacket(messageId, []byte(data), true)
 	if err != nil {
-		fmt.Println("client start failed, err:", err.Error())
-		return
-	}
-	i := 1
-	for {
-		//init packet
-		packTestData := packTestData(id)
-
-		//send
-		_, err := conn.Write(packTestData)
-		if err != nil {
-			break
-		}
-
-		//read
-		buf := make([]byte, 512)
-		cnt, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("read buf error ")
-			return
-		}
-		fmt.Printf(" server call back : %s, cnt = %d\n", string(buf), cnt)
-		if i >= 50 {
-			//up to limit of testing
-			break
-		}
-		time.Sleep(time.Second * 1)
-		i++
-	}
-	conn.Close()
-}
-
-func ClientTest(host string, port, clients int) {
-	time.Sleep(time.Second * 3)
-	addr := fmt.Sprintf("%s:%d", host, port)
-	for i := 1; i <= clients; i++ {
-		go CreateOneClient(addr, i)
+		log.Println("ClientWrite failed, err:", err.Error())
 	}
 }
 
@@ -74,14 +92,27 @@ func main() {
 		wg sync.WaitGroup
 		host = "127.0.0.1"
 		port = 7800
-		clients = 3
 	)
 
 	//wg
 	wg.Add(1)
 
+	//init new client
+	client := cree.NewClient(host, port)
+
+	//setup
+	client.SetCBForRead(CBForRead)
+
 	//try connect server
-	ClientTest(host, port, clients)
+	err := client.ConnServer()
+	if err != nil {
+		log.Println(err)
+		wg.Done()
+		os.Exit(1)
+	}
+
+	//spawn write testing
+	go ClientWrite(client)
 
 	wg.Wait()
 }
